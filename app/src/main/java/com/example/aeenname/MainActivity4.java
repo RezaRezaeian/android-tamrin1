2993d9fa6ecd3149b5d97a9e6702ea00c84361b3
Reza Rezaeian <rreza.rezaeian@gmail.com>
2023-05-09 22:48:07 +0330

    MainActivity4 changes

--- app/src/main/java/com/example/aeenname/MainActivity4.java
+++ app/src/main/java/com/example/aeenname/MainActivity4.java
@@ -2,13 +2,52 @@ package com.example.aeenname;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity4 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main4);
+        setContentView(R.layout.activity_main3);
+        RadioButton btnTrue=findViewById(R.id.radioButton4);
+        Button btnNext=findViewById(R.id.btnNext);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent recieve=getIntent();
+                int True=recieve.getIntExtra("true answer",0);
+                int False=recieve.getIntExtra("false answer",0);
+                int TestsNumber=recieve.getIntExtra("tests number",0);
+                TestsNumber++;
+                if (btnTrue.isChecked()) {
+                    ++True;
+                    Toast.makeText(MainActivity4.this, "صحیح", Toast.LENGTH_SHORT).show();
+                    Intent intentOk=new Intent (MainActivity4.this,MainActivity5.class);
+                    intentOk.putExtra("true answer",True);
+                    intentOk.putExtra("false answer",False);
+                    intentOk.putExtra("tests number",TestsNumber);
+                    startActivity(intentOk);
+                } else {
+                    ++False;
+                    Toast.makeText(MainActivity4.this, "غلط", Toast.LENGTH_LONG).show();
+                    Intent intentFalse =new Intent (MainActivity4.this,MainActivity5.class);
+                    intentFalse.putExtra("false answer",False);
+                    intentFalse.putExtra("true answer",True);
+                    intentFalse.putExtra("tests number",TestsNumber);
+                    startActivity(intentFalse);
+                }
+
+            }
+        });
     }
 }
