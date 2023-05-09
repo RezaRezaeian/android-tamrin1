331dcfee55cd5673226eaf5721e0777dddff480e
Reza Rezaeian <rreza.rezaeian@gmail.com>
2023-05-09 21:09:52 +0330

    MainActivity changes

--- app/src/main/java/com/example/aeenname/MainActivity.java
+++ app/src/main/java/com/example/aeenname/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.aeenname;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnStart = findViewById(R.id.btnStart);
+       // Button btnStart=findViewById(R.id.btnStart);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
